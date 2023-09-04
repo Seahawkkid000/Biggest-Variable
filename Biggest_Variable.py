a175cde030acbe4d09ce1cd05aaaf5a59ae08c47
Seahawkkid000 <107086227+Seahawkkid000@users.noreply.github.com>
2023-09-04 18:13:42 -0500

    Add files via upload

---
+++ Biggest_Variable.py
@@ -0,0 +1,10 @@
+
+a=int(input("Enter your first variable"))
+b=int(input("Enter your second variable"))
+c=int(input("Enter your third variable"))
+if (a>b and a>c):
+    print("The first variable was the biggest")
+elif (b>a and b>c):
+    print("The second variable was the biggest")
+elif (c>a and c>b):
+    print("The third variable was the biggest")
